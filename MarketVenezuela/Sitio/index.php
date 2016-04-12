19d51abc1bb07485cfd14d28728ac26e76ef7e4e
Junior <Junior@192.168.0.106>
2016-04-11 21:19:44 -0430

    Se comenzo a trabajar en la página principal!

--- MarketVenezuela/Sitio/index.php
+++ MarketVenezuela/Sitio/index.php
@@ -1,17 +1,182 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Market Venezuela</title>
+<style type="text/css">
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+#apDiv1 {
+	position:absolute;
+	left:0px;
+	top:0px;
+	width:100%;
+	height:214px;
+	z-index:1;
+}
+#apDiv2 {
+	position:absolute;
+	left:5%;
+	top:6px;
+	width:153px;
+	height:132px;
+	z-index:2;
+}
+#apDiv3 {
+	position:absolute;
+	left:0px;
+	top:200px;
+	width:100%;
+	height:38px;
+	z-index:3;
+}
+#apDiv4 {
+	position:absolute;
+	left:70%;
+	top:0%;
+	width:23%;
+	height:111px;
+	z-index:4;
+}
+#apDiv5 {
+	position:absolute;
+	left:8%;
+	top:210px;
+	width:85%;
+	height:26px;
+	z-index:5;
+}
+.Estilo1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+#apDiv6 {
+	position:absolute;
+	left:71%;
+	top:5px;
+	width:223px;
+	height:20px;
+	z-index:6;
+}
+body,td,th {
+	font-family: Arial, Helvetica, sans-serif;
+	color: #000000;
+}
+#apDiv7 {
+	position:absolute;
+	left:71%;
+	top:28px;
+	width:229px;
+	height:20px;
+	z-index:7;
+}
+.Estilo2 {
+	font-size: 12px;
+	font-weight: bold;
+}
+#apDiv8 {
+	position:absolute;
+	left:71%;
+	top:49px;
+	width:229px;
+	height:22px;
+	z-index:8;
+}
+#apDiv9 {
+	position:absolute;
+	left:71%;
+	top:72px;
+	width:228px;
+	height:16px;
+	z-index:9;
+}
+#apDiv10 {
+	position:absolute;
+	left:71%;
+	top:89px;
+	width:230px;
+	height:22px;
+	z-index:10;
+}
+#apDiv11 {
+	position:absolute;
+	left:8%;
+	top:242px;
+	width:85%;
+	height:313px;
+	z-index:11;
+	background-color: #CCCCCC;
+}
+#apDiv12 {
+	position:absolute;
+	left:8%;
+	top:558px;
+	width:85%;
+	height:45px;
+	z-index:12;
+}
+#apDiv13 {
+	position:absolute;
+	left:8%;
+	top:603px;
+	width:85%;
+	height:292px;
+	z-index:13;
+	background-color: #66CCFF;
+}
+#apDiv14 {
+	position:absolute;
+	left:0%;
+	top:946px;
+	width:100%;
+	height:234px;
+	z-index:14;
+	background-image: url(../Imagenes/barra_azul.png);
+}
+#apDiv15 {
+	position:absolute;
+	left:0%;
+	top:947px;
+	width:100%;
+	height:44px;
+	z-index:15;
+	background-image: url(../Imagenes/barra_menu.png);
+}
+#apDiv16 {
+	position:absolute;
+	left:8%;
+	top:997px;
+	width:225px;
+	height:149px;
+	z-index:16;
+}
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+</style>
+</head>
+
+<body>
+<div id="apDiv1"><img src="../Imagenes/barra_azul.png" width="100%" height="214" /></div>
+<div id="apDiv2"><img src="../Imagenes/Logo - Proyecto.png" width="202" height="198" /></div>
+<div id="apDiv3"><img src="../Imagenes/barra_menu.png" width="100%" height="39" /></div>
+<div id="apDiv4"><img src="../Imagenes/barra_menu.png" width="100%" height="121" /></div>
+<div class="Estilo1" id="apDiv5">Home / ¿Quienes Somos? / Productos / Sucursales</div>
+<div id="apDiv6"><strong>Iniciar Sesión</strong></div>
+<div class="Estilo2" id="apDiv7">Usuario:</div>
+<div id="apDiv8">
+  <label>
+  <input type="text" name="usuario" id="usuario" />
+  </label>
+</div>
+<div class="Estilo2" id="apDiv9">Contraseña:</div>
+<div id="apDiv10">
+  <label>
+  <input type="password" name="contrasena" id="contrasena" />
+  </label>
+</div>
+<div id="apDiv11"></div>
+<div id="apDiv12"><img src="../Imagenes/barra_menu.png" alt="" width="100%" height="47" /></div>
+<div id="apDiv13"></div>
+<div id="apDiv14"></div>
+<div id="apDiv15"></div>
+<div id="apDiv16"></div>
+</body>
 </html>
