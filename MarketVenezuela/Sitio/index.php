1e64332bc88fe536302034f66daf7a8f9aca0f45
José Francisco Hernández Balestrini <kikohernandez92@me.com>
2016-04-30 02:43:35 -0430

    Reubiqué todo para acomodarlo mejor, creé el CSS, Hay varios métodos de escalar las fotos, aún no corre ninguno, asumo porque se hizo HTML en un archivo .php. Igual sigo intentando.

--- MarketVenezuela/Sitio/index.php
+++ MarketVenezuela/Sitio/index.php
@@ -1,291 +1,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<script>document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';</script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Market Venezuela</title>
-<style type="text/css">
-<!--
-@media screen and (min-width: 0px) {
-  div#mydiv{
-	width:500px;
-	margin:0px auto;
-	height:100px; 
-	background: #D5E600;
-	font-size:27px;
-  }
-}
-@media screen and (min-width: 1000px) {
-  div#mydiv{
-	width:900px;
-	margin:0px auto;
-	height:100px; 
-	background: #D5E600;
-	font-size:27px;
-  }
-}
-#apDiv1 {
-	position:absolute;
-	left:0px;
-	top:0px;
-	width:100%;
-	height:214px;
-	z-index:1;
-}
-#apDiv2 {
-	position:absolute;
-	left:5%;
-	top:6px;
-	width:153px;
-	height:132px;
-	z-index:2;
-}
-#apDiv3 {
-	position:absolute;
-	left:0px;
-	top:200px;
-	width:100%;
-	height:38px;
-	z-index:3;
-}
-#apDiv4 {
-	position:absolute;
-	left:70%;
-	top:0%;
-	width:23%;
-	height:196px;
-	z-index:4;
-}
-#apDiv5 {
-	position:absolute;
-	left:8%;
-	top:210px;
-	width:85%;
-	height:26px;
-	z-index:5;
-}
-.Estilo1 {
-	font-family: Arial, Helvetica, sans-serif;
-	font-weight: bold;
-}
-#apDiv6 {
-	position:absolute;
-	left:71%;
-	top:5px;
-	width:223px;
-	height:20px;
-	z-index:6;
-}
-body,td,th {
-	font-family: Arial, Helvetica, sans-serif;
-	color: #000000;
-}
-#apDiv7 {
-	position:absolute;
-	left:71%;
-	top:28px;
-	width:229px;
-	height:20px;
-	z-index:7;
-}
-.Estilo2 {
-	font-size: 12px;
-	font-weight: bold;
-}
-#apDiv8 {
-	position:absolute;
-	left:71%;
-	top:49px;
-	width:152px;
-	height:22px;
-	z-index:8;
-}
-#apDiv9 {
-	position:absolute;
-	left:71%;
-	top:72px;
-	width:228px;
-	height:16px;
-	z-index:9;
-}
-#apDiv10 {
-	position:absolute;
-	left:71%;
-	top:89px;
-	width:155px;
-	height:22px;
-	z-index:10;
-}
-#apDiv11 {
-	position:absolute;
-	left:8%;
-	top:242px;
-	width:85%;
-	height:313px;
-	z-index:11;
-	background-color: #FFFFFF;
-}
-#apDiv12 {
-	position:absolute;
-	left:8%;
-	top:570px;
-	width:85%;
-	height:45px;
-	z-index:12;
-}
-#apDiv13 {
-	position:absolute;
-	left:8%;
-	top:603px;
-	width:85%;
-	height:292px;
-	z-index:13;
-	background-color: #66CCFF;
-}
-#apDiv14 {
-	position:absolute;
-	left:0%;
-	top:1090px;
-	width:100%;
-	height:234px;
-	z-index:14;
-	background-image: url(../Imagenes/barra_azul.png);
-}
-#apDiv15 {
-	position:absolute;
-	left:0%;
-	top:1090px;
-	width:100%;
-	height:44px;
-	z-index:15;
-	background-image: url(../Imagenes/barra_menu.png);
-}
-#apDiv16 {
-	position:absolute;
-	left:8%;
-	top:1140px;
-	width:134px;
-	height:149px;
-	z-index:16;
-}
-#apDiv17 {
-	position:absolute;
-	left:29%;
-	top:1140px;
-	width:556px;
-	height:58px;
-	z-index:17;
-}
-.Estilo3 {font-size: 10px}
-.Estilo4 {font-size: 18px}
-.Estilo5 {font-size: 18}
-.Estilo6 {color: #FFFFFF}
-.Estilo7 {font-size: 18; color: #FFFFFF; }
-#apDiv18 {
-	position:absolute;
-	left:26%;
-	top:1280px;
-	width:685px;
-	height:31px;
-	z-index:18;
-}
-#apDiv19 {
-	position:absolute;
-	left:8%;
-	top:607px;
-	width:401px;
-	height:239px;
-	z-index:19;
-}
-#apDiv20 {
-	position:absolute;
-	left:38%;
-	top:607px;
-	width:397px;
-	height:237px;
-	z-index:20;
-}
-#apDiv21 {
-	position:absolute;
-	left:939px;
-	top:118px;
-	width:262px;
-	height:33px;
-	z-index:21;
-}
-#apDiv22 {
-	position:absolute;
-	left:71%;
-	top:138px;
-	width:288px;
-	height:20px;
-	z-index:21;
-}
-#apDiv23 {
-	position:absolute;
-	left:71%;
-	top:162px;
-	width:286px;
-	height:22px;
-	z-index:22;
-}
-#apDiv24 {
-	position:absolute;
-	left:84%;
-	top:70px;
-	width:112px;
-	height:44px;
-	z-index:23;
-}
-#apDiv25 {
-	position:absolute;
-	left:1093px;
-	top:88px;
-	width:121px;
-	height:26px;
-	z-index:23;
-}
-#apDiv26 {
-	position:absolute;
-	left:85%;
-	top:90px;
-	width:104px;
-	height:29px;
-	z-index:23;
-}
-#apDiv27 {
-	position:absolute;
-	left:67.9%;
-	top:607px;
-	width:340px;
-	height:476px;
-	z-index:25;
-}
-#apDiv28 {
-	position:absolute;
-	left:8%;
-	top:844px;
-	width:404px;
-	height:231px;
-	z-index:26;
-}
-#apDiv29 {
-	position:absolute;
-	left:38%;
-	top:844px;
-	width:384px;
-	height:238px;
-	z-index:24;
-}
--->
-</style>
+<script type="text/javascript">document.cookie='resolution='+Math.max(screen.width,screen.height)+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
+<link rel="stylesheet" href="../Sitio/css/home.css" type="text/css">
+<link rel="stylesheet" href="../Sitio/css/media-queries.css" type="text/css">
 </head>
 
 <body>
-<div id="apDiv1"><img src="../Imagenes/barra_azul.png" width="100%" height="214" /></div>
-<div id="apDiv2"><img src="../Imagenes/Logo - Proyecto.png" width="202" height="198" /></div>
-<div id="apDiv3"><img src="../Imagenes/barra_menu.png" width="100%" height="39" /></div>
-<div id="apDiv4"><img src="../Imagenes/barra_menu.png" width="100%" height="191" /></div>
+<div id="apDiv1"><img src="../Sitio/Imagenes/barra_azul.png" width="100%" height="214" /></div>
+<div id="apDiv2"><img src="../Sitio/Imagenes/Logo - Proyecto.png" width="202" height="198" /></div>
+<div id="apDiv3"><img src="../Sitio/Imagenes/barra_menu.png" width="100%" height="39" /></div>
+<div id="apDiv4"><img src="../Sitio/Imagenes/barra_menu.png" width="100%" height="191" /></div>
 <div class="Estilo1" id="apDiv5">Home / ¿Quienes Somos? / Productos / Sucursales</div>
 <div id="apDiv6"><strong>Iniciar Sesión</strong>
 </div>
@@ -304,21 +31,21 @@ body,td,th {
 <div id="apDiv11">
 <div align="center">
   <iframe frameborder="NO" height="375" marginheight="0" marginwidth="0" scrolling="YES" src="Panel/Panel.html" style="border-color: black; border-style: solid; border-width: 5;" width="1100">
-  </iframe>
+	</iframe>
     </div>
 </div>
-<div id="apDiv12"><img src="../Imagenes/barra_menu.png" alt="" width="100%" height="47" /></div>
+<div id="apDiv12"><img src="../Sitio/Imagenes/barra_menu.png" alt="" width="100%" height="47" /></div>
 <div id="apDiv14"></div>
 <div id="apDiv15"></div>
-<div id="apDiv16"><img src="../Imagenes/ucab.png" alt="ucab" width="127" height="151" /></div>
+<div id="apDiv16"><img src="../Sitio/Imagenes/ucab.png" alt="ucab" width="127" height="151" /></div>
 <div id="apDiv17">
   <p align="center"><span class="Estilo3"><span class="Estilo4"><span class="Estilo5"><span class="Estilo6"><strong>Teléfonos:</strong> 0212-234.23.32 / 0212.374.44.23</span></span></span></span></p>
   <p align="center" class="Estilo7"><strong>Correo: </strong>info@marketvenezuela.com</p>
   <p align="center" class="Estilo7">Todos los Derechos Reservados - 2016</p>
 </div>
 <div id="apDiv18"><strong>Desarrollo y Mantenimiento: Garry Jr Bruno M. / Mario Santana / Jose F. Hernández</strong></div>
-<div id="apDiv19"><img src="../Imagenes/publi1.png" alt="oferta1" width="404" height="237" /></div>
-<div id="apDiv20"><img src="../Imagenes/publi2.png" alt="publi2" width="402" height="237" /></div>
+<div id="apDiv19"><img src="../Sitio/Imagenes/publi1.png" alt="oferta1" width="404" height="237" /></div>
+<div id="apDiv20"><img src="../Sitio/Imagenes/publi2.png" alt="publi2" width="402" height="237" /></div>
 <div class="Estilo2" id="apDiv22">
   <div align="center">¿No tienes cuenta?</div>
 </div>
@@ -328,8 +55,8 @@ body,td,th {
 <div id="apDiv26">
   <input type="button" name="iniciarsesion" id="iniciarsesion" value="Iniciar Sesion" />
 </div>
-<div id="apDiv27"><img src="../Imagenes/publi3.png" alt="publi3" width="100%" height="474" /></div>
-<div id="apDiv28"><img src="../Imagenes/publi4.png" alt="publi4" width="404" height="237" /></div>
-<div id="apDiv29"><img src="../Imagenes/publi5.png" alt="publi5" width="404" height="237" /></div>
+<div id="apDiv27"><img src="../Sitio/Imagenes/publi3.png" alt="publi3" width="100%" height="474" /></div>
+<div id="apDiv28"><img src="../Sitio/Imagenes/publi4.png" alt="publi4" width="404" height="237" /></div>
+<div id="apDiv29"><img src="../Sitio/Imagenes/publi5.png" alt="publi5" width="404" height="237" /></div>
 </body>
 </html>
