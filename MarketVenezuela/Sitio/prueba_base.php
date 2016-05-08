ef5155cb8e7df5ae1de9dcdec19d479ae7ebb5e5
gjbm95 <Junior@192.168.0.101>
2016-05-08 11:11:50 -0400

    Se creo todas las imagenes de los principales botones con sus figuras.

---
+++ MarketVenezuela/Sitio/prueba_base.php
@@ -0,0 +1,19 @@
+<?php
+//  $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=ingenieriasoftware2015");
+// 
+// 
+//   $result = pg_query($conn, "SELECT nombre, codigo, precio FROM public.inventario;");
+//		if (!$result) {
+//		  echo "An error occurred.\n";
+//		  exit;
+//		}
+//
+//		while ($row = pg_fetch_row($result)) {
+//		  echo "Nombre: $row[0]  Precio: $row[2]";
+//		  echo "<br />\n";
+//		}
+
+phpinfo();
+
+
+?>
