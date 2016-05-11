a325117c285f113e44760f5e78bbcf828b518fac
gjbm95 <Junior@192.168.0.101>
2016-05-11 16:48:47 -0400

    parte de mario

---
+++ MarketVenezuela/Sitio/agregar_proveedor.php
@@ -0,0 +1,867 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Market Venezuela</title>
+
+
+<style type="text/css">
+<!--
+#apDiv1 {
+	position:absolute;
+	left:0px;
+	top:0px;
+	width:100%;
+	height:214px;
+	z-index:2;
+}
+#apDiv2 {
+	position:absolute;
+	left:5%;
+	top:0px;
+	width:153px;
+	height:132px;
+	z-index:3;
+}
+#apDiv3 {
+	position:absolute;
+	left:0px;
+	top:200px;
+	width:100%;
+	height:38px;
+	z-index:4;
+}
+#apDiv4 {
+	position:absolute;
+	left:70%;
+	top:0px;
+	width:23%;
+	height:196px;
+	z-index:5;
+}
+#apDiv5 {
+	position:absolute;
+	left:8%;
+	top:210px;
+	width:85%;
+	height:26px;
+	z-index:6;
+}
+.Estilo1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+#apDiv6 {
+	position:absolute;
+	left:71%;
+	top:5px;
+	width:223px;
+	height:20px;
+	z-index:7;
+}
+body,td,th {
+	font-family: Arial, Helvetica, sans-serif;
+	color: #000000;
+}
+#apDiv7 {
+	position:absolute;
+	left:71%;
+	top:28px;
+	width:229px;
+	height:20px;
+	z-index:8;
+}
+.Estilo2 {
+	font-size: 12px;
+	font-weight: bold;
+}
+#apDiv8 {
+	position:absolute;
+	left:71%;
+	top:49px;
+	width:152px;
+	height:22px;
+	z-index:9;
+}
+#apDiv9 {
+	position:absolute;
+	left:71%;
+	top:72px;
+	width:228px;
+	height:16px;
+	z-index:10;
+}
+#apDiv10 {
+	position:absolute;
+	left:71%;
+	top:89px;
+	width:155px;
+	height:22px;
+	z-index:11;
+}
+#apDiv11 {
+	position:absolute;
+	left:0%;
+	top:239px;
+	width:100%;
+	height:103px;
+	z-index:12;
+	background-color: #FFFFFF;
+}
+#apDiv12 {
+	position:absolute;
+	left:0%;
+	top:320px;
+	width:100%;
+	height:45px;
+	z-index:13;
+}
+#apDiv13 {
+	position:absolute;
+	left:8%;
+	top:603px;
+	width:85%;
+	height:292px;
+	z-index:13;
+	background-color: #66CCFF;
+}
+#apDiv14 {
+	position:absolute;
+	left:0%;
+	top:1090px;
+	width:100%;
+	height:234px;
+	z-index:14;
+	background-image: url(Imagenes/barra_azul.png);
+}
+#apDiv15 {
+	position:absolute;
+	left:0%;
+	top:1090px;
+	width:100%;
+	height:44px;
+	z-index:15;
+	background-image: url(Imagenes/barra_menu.png);
+}
+#apDiv16 {
+	position:absolute;
+	left:8%;
+	top:1140px;
+	width:134px;
+	height:149px;
+	z-index:16;
+}
+#apDiv17 {
+	position:absolute;
+	left:29%;
+	top:1140px;
+	width:556px;
+	height:58px;
+	z-index:17;
+}
+.Estilo3 {font-size: 10px}
+.Estilo4 {font-size: 18px}
+.Estilo5 {font-size: 18}
+.Estilo6 {color: #FFFFFF}
+.Estilo7 {font-size: 18; color: #FFFFFF; }
+#apDiv18 {
+	position:absolute;
+	left:26%;
+	top:1280px;
+	width:685px;
+	height:31px;
+	z-index:18;
+}
+#apDiv19 {
+	position:absolute;
+	left:8%;
+	top:607px;
+	width:401px;
+	height:239px;
+	z-index:19;
+}
+
+#apDiv20 {
+	position:absolute;
+	left:38%;
+	top:607px;
+	width:397px;
+	height:237px;
+	z-index:20;
+}
+#apDiv21 {
+	position:absolute;
+	left:939px;
+	top:118px;
+	width:262px;
+	height:33px;
+	z-index:21;
+}
+#apDiv22 {
+	position:absolute;
+	left:71%;
+	top:138px;
+	width:288px;
+	height:20px;
+	z-index:21;
+}
+#apDiv23 {
+	position:absolute;
+	left:71%;
+	top:162px;
+	width:286px;
+	height:22px;
+	z-index:22;
+}
+#apDiv24 {
+	position:absolute;
+	left:84%;
+	top:70px;
+	width:112px;
+	height:44px;
+	z-index:23;
+}
+#apDiv25 {
+	position:absolute;
+	left:1093px;
+	top:88px;
+	width:121px;
+	height:26px;
+	z-index:23;
+}
+#apDiv26 {
+	position:absolute;
+	left:85%;
+	top:90px;
+	width:104px;
+	height:29px;
+	z-index:23;
+}
+#apDiv27 {
+	position:absolute;
+	left:67.9%;
+	top:607px;
+	width:340px;
+	height:476px;
+	z-index:25;
+}
+#apDiv28 {
+	position:absolute;
+	left:8%;
+	top:844px;
+	width:404px;
+	height:231px;
+	z-index:26;
+}
+#apDiv29 {
+	position:absolute;
+	left:38%;
+	top:844px;
+	width:384px;
+	height:238px;
+	z-index:24;
+}
+#apDiv30 {
+	position:absolute;
+	left:8%;
+	top:369px;
+	width:85%;
+	height:696px;
+	z-index:24;
+	background-image: url(Imagenes/barra_azul.png);
+}
+#apDiv31 {
+	position:absolute;
+	left:8%;
+	top:253px;
+	width:911px;
+	height:69px;
+	z-index:25;
+}
+.Estilo8 {
+	font-size: 36px;
+	color: #FFFFFF;
+	font-weight: bold;
+}
+#apDiv32 {
+	position:absolute;
+	left:12%;
+	top:420px;
+	width:386px;
+	height:41px;
+	z-index:26;
+}
+#apDiv33 {
+	position:absolute;
+	left:12%;
+	top:471px;
+	width:125px;
+	height:28px;
+	z-index:27;
+}
+#apDiv34 {
+	position:absolute;
+	left:30%;
+	top:471px;
+	width:140px;
+	height:27px;
+	z-index:28;
+}
+#apDiv35 {
+	position:absolute;
+	left:49%;
+	top:471px;
+	width:121px;
+	height:27px;
+	z-index:29;
+}
+#apDiv36 {
+	position:absolute;
+	left:68%;
+	top:471px;
+	width:143px;
+	height:28px;
+	z-index:30;
+}
+#apDiv37 {
+	position:absolute;
+	left:12%;
+	top:511px;
+	width:212px;
+	height:34px;
+	z-index:31;
+}
+#apDiv38 {
+	position:absolute;
+	left:30%;
+	top:511px;
+	width:212px;
+	height:55px;
+	z-index:32;
+}
+#apDiv39 {
+	position:absolute;
+	left:49%;
+	top:511px;
+	width:207px;
+	height:60px;
+	z-index:33;
+}
+#apDiv40 {
+	position:absolute;
+	left:68%;
+	top:511px;
+	width:225px;
+	height:63px;
+	z-index:34;
+}
+#apDiv41 {
+	position:absolute;
+	left:8%;
+	top:1014px;
+	width:85%;
+	height:71px;
+	z-index:26;
+	background-color: #FFCC33;
+}
+#apDiv42 {
+	position:absolute;
+	left:9%;
+	top:78px;
+	width:225px;
+	height:175px;
+	z-index:27;
+	top: 910px;
+}
+#apDiv43 {
+	position:absolute;
+	left:8%;
+	top:370px;
+	width:287px;
+	height:721px;
+	z-index:26;
+	background-image: url(Imagenes/barra_menu.png);
+}
+#apDiv44 {
+	position:absolute;
+	left:8%;
+	top:372px;
+	width:280px;
+	height:221px;
+	z-index:27;
+}
+#apDiv45 {
+	position:absolute;
+	left:30%;
+	top:374px;
+	width:376px;
+	height:25px;
+	z-index:28;
+}
+#apDiv46 {
+	position:absolute;
+	left:30%;
+	top:410px;
+	width:807px;
+	height:157px;
+	z-index:29;
+}
+#apDiv47 {
+	position:absolute;
+	left:8%;
+	top:603px;
+	width:285px;
+	height:40px;
+	z-index:30;
+}
+#apDiv48 {
+	position:absolute;
+	left:9%;
+	top:680px;
+	width:257px;
+	height:68px;
+	z-index:31;
+}
+#apDiv49 {
+	position:absolute;
+	left:9%;
+	top:759px;
+	width:257px;
+	height:64px;
+	z-index:32;
+}
+#apDiv50 {
+	position:absolute;
+	left:9%;
+	top:837px;
+	width:257px;
+	height:72px;
+	z-index:33;
+}
+#apDiv51 {
+	position:absolute;
+	left:9%;
+	top:919px;
+	width:258px;
+	height:63px;
+	z-index:34;
+}
+#apDiv52 {
+	position:absolute;
+	left:8%;
+	top:369px;
+	width:85%;
+	height:182px;
+	z-index:35;
+}
+#apDiv53 {
+	position:absolute;
+	left:30%;
+	top:784px;
+	width:418px;
+	height:251px;
+	z-index:36;
+}
+#apDiv54 {
+	position:absolute;
+	left:62%;
+	top:784px;
+	width:408px;
+	height:250px;
+	z-index:37;
+}
+#apDiv55 {
+	position:absolute;
+	left:63%;
+	top:410px;
+	width:387px;
+	height:155px;
+	z-index:38;
+}
+#apDiv58 {	position:absolute;
+	left:111px;
+	top:329px;
+	width:238px;
+	height:29px;
+	z-index:41;
+}
+#apDiv56 {
+	position:absolute;
+	left:9%;
+	top:658px;
+	width:262px;
+	height:62px;
+	z-index:42;
+}
+#apDiv57 {
+	position:absolute;
+	left:9%;
+	top:731px;
+	width:263px;
+	height:67px;
+	z-index:43;
+}
+#apDiv59 {
+	position:absolute;
+	left:9%;
+	top:804px;
+	width:263px;
+	height:65px;
+	z-index:44;
+}
+#apDiv60 {
+	position:absolute;
+	left:9%;
+	top:876px;
+	width:263px;
+	height:71px;
+	z-index:45;
+}
+#apDiv61 {
+	position:absolute;
+	left:9%;
+	top:950px;
+	width:264px;
+	height:65px;
+	z-index:46;
+}
+#apDiv62 {
+	position:absolute;
+	left:9%;
+	top:1020px;
+	width:264px;
+	height:52px;
+	z-index:47;
+}
+#apDiv63 {
+	position:absolute;
+	left:15%;
+	top:540px;
+	width:216px;
+	height:25px;
+	z-index:42;
+}
+#apDiv64 {
+	position:absolute;
+	left:15%;
+	top:580px;
+	width:275px;
+	height:28px;
+	z-index:43;
+}
+#apDiv76 {
+	position:absolute;
+	left:51%;
+	top:580px;
+	width:396px;
+	height:210px;
+	z-index:56;
+	background-color: #FFCC00;
+}
+#apDiv65 {
+	position:absolute;
+	left:678px;
+	top:660px;
+	width:275px;
+	height:26px;
+	z-index:44;
+}
+#apDiv66 {
+	position:absolute;
+	left:51%;
+	top:682px;
+	width:342px;
+	height:51px;
+	z-index:45;
+}
+#apDiv67 {
+	position:absolute;
+	left:15%;
+	top:620px;
+	width:236px;
+	height:28px;
+	z-index:46;
+}
+#apDiv68 {
+	position:absolute;
+	left:15%;
+	top:650px;
+	width:274px;
+	height:29px;
+	z-index:47;
+}
+#apDiv69 {
+	position:absolute;
+	left:15%;
+	top:900px;
+	width:309px;
+	height:27px;
+	z-index:48;
+}
+#apDiv70 {
+	position:absolute;
+	left:15%;
+	top:930px;
+	width:215px;
+	height:25px;
+	z-index:52;
+}
+#apDiv71 {
+	position:absolute;
+	left:32%;
+	top:930px;
+	width:1px;
+	height:58px;
+	z-index:53;
+}
+#apDiv72 {
+	position:absolute;
+	left:51%;
+	top:900;
+	width:1px;
+	height:59px;
+	z-index:63;
+}
+#apDiv73 {
+	position:absolute;
+	left:69%;
+	top:930px;
+	width:206px;
+	height:63px;
+	z-index:64;
+}
+#apDiv74 {
+	position:absolute;
+	left:41%;
+	top:960px;
+	width:234px;
+	height:40px;
+	z-index:54;
+}
+#apDiv75 {
+	position:absolute;
+	left:8%;
+	top:982px;
+	width:85%;
+	height:83px;
+	z-index:55;
+	background-color: #FFCC33;
+}
+#apDiv77 {
+	position:absolute;
+	left:53%;
+	top:586px;
+	width:230px;
+	height:27px;
+	z-index:57;
+}
+#apDiv78 {
+	position:absolute;
+	left:53%;
+	top:622px;
+	width:369px;
+	height:155px;
+	z-index:57;
+}
+#Layer1 {
+	position:absolute;
+	left:15%;
+	top:690px;
+	width:268px;
+	height:23px;
+	z-index:59;
+}
+#Layer2 {
+	position:absolute;
+	left:15%;
+	top:720px;
+	width:280px;
+	height:24px;
+	z-index:60;
+}
+#Layer3 {
+	position:absolute;
+	left:15%;
+	top:760px;
+	width:312px;
+	height:24px;
+	z-index:61;
+}
+#Layer4 {
+	position:absolute;
+	left:15%;
+	top:790px;
+	width:317px;
+	height:17px;
+	z-index:62;
+}
+#Layer5 {
+	position:absolute;
+	left:15%;
+	top:830px;
+	width:322px;
+	height:19px;
+	z-index:58;
+}
+#Layer6 {
+	position:absolute;
+	left:51%;
+	top:91;
+	width:128px;
+	height:41px;
+	z-index:65;
+}
+#Layer7 {
+	position:absolute;
+	left:51%;
+	top:930px;
+	width:167px;
+	height:40px;
+	z-index:66;
+}
+#Layer8 {
+	position:absolute;
+	left:15%;
+	top:860px;
+	width:272px;
+	height:31px;
+	z-index:67;
+}
+#Layer9 {
+	position:absolute;
+	left:51%;
+	top:830px;
+	width:391px;
+	height:25px;
+	z-index:68;
+}
+#Layer10 {
+	position:absolute;
+	left:51%;
+	top:860px;
+	width:417px;
+	height:24px;
+	z-index:69;
+}
+-->
+</style>
+</head>
+
+<body>
+<div id="apDiv1"><img src="../Sitio/Imagenes/barra_azul.png" width="100%" height="214" /></div>
+<div id="apDiv2"><a href="index.php"><img src="../Sitio/Imagenes/Logo - Proyecto.png" width="202" height="198" /></a></div>
+<div id="apDiv3"><img src="../Sitio/Imagenes/barra_menu.png" width="100%" height="39" /></div>
+<div id="apDiv4"><img src="../Sitio/Imagenes/barra_menu.png" width="100%" height="191" /></div>
+<div class="Estilo1" id="apDiv5"><a href="index.php">Home</a> / ¿Quienes Somos? / Productos / Sucursales</div>
+
+<div id="apDiv6"><strong>Iniciar Sesión</strong></div>
+<div class="Estilo2" id="apDiv7">Usuario:</div>
+<div id="apDiv8">
+  <label>
+  <input type="text" name="usuario" id="usuario" />
+  </label>
+</div>
+<div class="Estilo2" id="apDiv9">Contraseña:</div>
+<div id="apDiv10">
+  <label>
+  <input type="password" name="contrasena" id="contrasena" />
+  </label>
+</div>
+<div id="apDiv11"><img src="../Sitio/Imagenes/barra_azul.png" alt="" width="100%" height="102" /></div>
+<div id="apDiv12"><img src="../Sitio/Imagenes/barra_menu.png" alt="" width="100%" height="47" /></div>
+<div id="apDiv14"></div>
+<div id="apDiv15"></div>
+<div id="apDiv16"><img src="../Sitio/Imagenes/ucab.png" alt="ucab" width="127" height="151" /></div>
+<div id="apDiv17">
+  <p align="center"><span class="Estilo3"><span class="Estilo4"><span class="Estilo5"><span class="Estilo6"><strong>Teléfonos:</strong> 0212-234.23.32 / 0212-374.44.23</span></span></span></span></p>
+  <p align="center" class="Estilo7"><strong>Correo: </strong>info@marketvenezuela.com</p>
+  <p align="center" class="Estilo7">Todos los Derechos Reservados - 2016</p>
+</div>
+<div id="apDiv18"><strong>Desarrollo y Mantenimiento: Garry Jr Bruno M. / Mario Santana / Jose F. Hernández</strong></div>
+<div class="Estilo2" id="apDiv22">
+  <div align="center">¿No tienes cuenta?</div>
+</div>
+<div id="apDiv23">
+  <div align="center" class="Estilo2"><a href="registro.php">Registrate</a></div>
+</div>
+<div id="apDiv26">
+  <input type="submit" name="iniciarsesion" id="iniciarsesion" value="Iniciar Sesion" />
+</div>
+<div id="apDiv30">  </div>
+<div class="Estilo8" id="apDiv31">Agregar Proveedor </div>
+<div id="apDiv52"><img src="Imagenes/Figura - Sucursal1.png" width="100%" height="182" /></div>
+<div id="apDiv58"><strong><a href="gestion_proveedores.php">Inicio</a></strong></div>
+<div class="Estilo6" id="apDiv63">
+  <p>Razon Social :</p>
+</div>
+<div id="apDiv64">
+  <form id="form1" name="form1" method="post" action="">
+    <input name="textfield" type="text" id="textfield" size="30" />
+  </form>
+</div>
+<div class="Estilo6" id="apDiv67">	RIF:</div>
+<div id="apDiv68">
+  <form id="form3" name="form3" method="post" action="">
+    <input name="textfield2" type="text" id="textfield2" size="30" />
+  </form>
+</div>
+<div class="Estilo6" id="apDiv69">Dirección Geográfica:</div>
+<div id="apDiv70">
+  <form id="form4" name="form4" method="post" action="">
+    <select name="select" id="select">
+      <option>Seleccione Estado</option>
+    </select>
+  </form>
+</div>
+<div id="apDiv71">
+  <form id="form5" name="form5" method="post" action="">
+    <select name="select2" id="select2">
+      <option>Seleccione Ciudad</option>
+    </select>
+  </form>
+</div>
+<div id="apDiv73">
+  <form id="form7" name="form7" method="post" action="">
+    <select name="select4" id="select4">
+      <option>Seleccione Parroquia</option>
+    </select>
+  </form>
+</div>
+<div id="apDiv74">
+  <form id="form8" name="form8" method="post" action="">
+    <div align="center">
+      <input type="reset" name="button" id="button" value="Agregar Proveedor" />
+    </div>
+  </form>
+</div>
+<div id="apDiv75"></div>
+<div id="apDiv76"></div>
+<div id="apDiv77"><strong>Logo</strong></div>
+<div id="Layer1"><span class="Estilo6">Denominacion Comercial: </span></div>
+<div id="Layer2">
+  <input name="textfield22" type="text" id="textfield22" size="30" />
+</div>
+<div id="Layer3"><span class="Estilo6">Rubro:</span></div>
+<div id="Layer4">
+  <select name="select5" id="select5">
+    <option>Seleccione Rubro</option>
+        </select>
+</div>
+<div class="Estilo6" id="Layer5">Pagina Web: </div>
+<div id="Layer6"></div>
+<div id="Layer7">
+  <select name="select3" id="select3">
+    <option>Seleccione Municipio</option>
+    </select>
+</div>
+<div id="Layer8">
+  <input name="textfield222" type="text" id="textfield222" size="30" />
+</div>
+<div class="Estilo6" id="Layer9">Status:</div>
+<div id="Layer10">
+  <select name="select6" id="select6">
+    <option>Seleccione Status</option>
+    </select>
+</div>
+</body>
+</html>
